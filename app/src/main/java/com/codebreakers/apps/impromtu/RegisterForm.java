bf1497e9fc334c4b894c56040397cc1ed81aa2d2
ajaysjacob <ajaysjacob@gmail.com>
2017-02-04 09:43:30 +0530

    Added Register Form

---
+++ app/src/main/java/com/codebreakers/apps/impromtu/RegisterForm.java
@@ -0,0 +1,13 @@
+package com.codebreakers.apps.impromtu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_form);
+    }
+}
